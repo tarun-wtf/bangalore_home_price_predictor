b73e4e4fc20e0d5b63da39d880db9c884c68d10f
Tarun <TARUNPATTUBALA@GMAIL.COM>
2026-01-14 22:18:33 +0530

    Update util.py

--- util.py
+++ util.py
@@ -34,4 +34,39 @@ def get_estimated_price(location, sqft, bhk, bath):
         x[loc_index] = 1
 
     # Prediction
-    return round(__mod
+    return round(__model.predict([x])[0], 2)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    columns_path = os.path.join(ARTIFACTS_DIR, "columns.json")
+    model_path = os.path.join(ARTIFACTS_DIR, "banglore_home_prices_model.pickle")
+
+    # Load columns.json
+    with open(columns_path, "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # All locations from index 3 onward
+
+    # Load model only once
+    if __model is None:
+        with open(model_path, "rb") as f:
+            __model = pickle.load(f)
+
+    print("loading saved artifacts...done")
+
+
+def get_location_names():
+    return __locations
+
+
+def get_data_columns():
+    return __data_columns
+
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
